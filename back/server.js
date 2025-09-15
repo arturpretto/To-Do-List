09e3691875fef77f8d65e480f7c75979d97d57fc
Artur Andrioli Pretto <arturpretto@outlook.com>
2025-09-14 22:25:12 -0300

    user requests

--- back/server.js
+++ back/server.js
@@ -1,38 +1,67 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import Prisma from '@prisma/client';
+const { PrismaClient } = Prisma;
 
-const app = express();
 const prisma = new PrismaClient()
+const app = express();
 
 app.use(express.json())
+
+app.post('/User', async (req, res) => {
+    const userData = req.body
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            password: req.body.password
+        }
+    })
+    
+    res.status(201).json({ message: 'Usuário criado.', data: userData })
+})
+
+app.get('/User', async (req, res) => {
+    let users = []
+
+    if (req.query) {
+        users = await prisma.user.findMany({
+            where: {
+            email: req.query.email,
+            name: req.query.name,
+            password: req.query.password
+            }
+    })
+    }
+
+    res.status(200).json({ message: 'Usuários: ', data: users })
+})
+
+app.delete('/User/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: 'Usuário deletado.' })
+})
+
+app.put('/User/:id', async (req, res) => {
+    const userData = req.body
+
+    await prisma.user.put({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            password: req.body.password
+        }
+    })
+
+    res.status(201).json({ message: 'Usuário atualizado.', data: userData })
+})
+
 app.listen(3000)
-
-app.post('/User', (req, res) => {
-    const userData = req.body
-
-    console.log('Dados recebidos: ', userData)
-
-    res.json({ message: 'Usuário criado.', data: userData })
-})
-
-app.get('/User', (req, res) => {
-    const users = req.body
-
-    console.log('Usuários: ', users)
-
-    res.json({ message: 'Usuários: ', data: users })
-})
-
-app.delete('/User/:id', (req, res) => {
-    console.log('Dados removidos. ')
-
-    res.json({ message: 'Usuário deletado.' })
-})
-
-app.put('/User/:id', (req, res) => {
-    const userData = req.body
-
-    console.log('Dados atualizados: ', userData)
-
-    res.json({ message: 'Usuário atualizado.', data: userData })
-})
